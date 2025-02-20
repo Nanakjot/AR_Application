40ee50dfdd2ad6ea70a46c8c9ce98a1a653ce6ff
Nanakjot <nanak006chahal@gmail.com>
2025-02-20 11:33:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ar_application/SignUp.java
@@ -0,0 +1,32 @@
+package com.example.ar_application;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignUp extends AppCompatActivity {
+
+    public void loginBtn(View view){
+        Intent land = new Intent(this, MainActivity.class);
+        startActivity(land);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
